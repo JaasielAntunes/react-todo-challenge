8fc6bcb1acd9e1db8974626ecc68652025db7b0e
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-03-25 17:14:23 -0300

    Alterações no layout do botão de criar tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './global.css'
 import styles from './App.module.css'
 import { Header } from './components/Header'
 import { Input } from './components/Input'
+import { Button } from './components/Button'
+import { PlusCircle } from 'phosphor-react'
 
 function App() {
   return (
@@ -12,6 +14,11 @@ function App() {
         <section className={styles.content}>
           <div className={styles.taskInfoContainer}>
             <Input />
+
+            <Button>
+              Criar
+              <PlusCircle size={16} color="#f2f2f2" weight="bold" />
+            </Button>
           </div>
         </section>
       </main>
